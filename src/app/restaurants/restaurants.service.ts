7b32bf69b7b80d26717c0103620eb1b39eedf462
Jefter Alves <jefteralveskf2@gmail.com>
2021-07-02 21:43:55 -0300

    Trabalhando com detalhes do restaurante, rotas filhas, pipes, carrinho de compras e template forms.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,10 +6,11 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import { Restaurant } from "./restaurant/restaurant.model"
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item-model'
 
 import { MEAT_API } from '../app.api'
-
 import { ErrorHandler } from '../app.error-handler'
+
 
 @Injectable()
 
@@ -25,6 +26,18 @@ export class RestaurantsService {
 
     restaurantById(id: string): Observable<Restaurant> {
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
